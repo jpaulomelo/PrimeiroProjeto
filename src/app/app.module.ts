010f5faf1d2eb859cfcd1c9168ff49368cc246fb
jpaulo.melo <jpaulo.melo@gmail.com>
2018-06-03 10:24:54 -0300

    Criado modulo cursos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component/meuprimeirocomponent';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuSegundoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // 
+  bootstrap: [AppComponent]  // App inicial do projeto, controladora geral
 })
 export class AppModule { }
